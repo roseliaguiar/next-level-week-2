602380f6dcedebe6a483a70637c347b2669b22c4
roseliaguiar <62181263+roseliaguiar@users.noreply.github.com>
2020-08-05 14:34:14 -0300

    third day

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,11 +1,15 @@
-//PROUCURAR O BOTÃO
-//QUANDO CLICAR NO BOTÃO 
-//EXECUTAR UMA ACAO 
-//DUPLICAR OS CAMPOS 
-//COLOCAR NA PAGINA
-
+//ADICIONA MAIS CAMPOS NO HORARIO
 document.querySelector('#add-time').addEventListener('click', cloneField)
 
 function cloneField() {
-    document.querySelector('.schedule-item')
+    const newfieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    const fields = newfieldContainer.querySelectorAll('input')
+
+    //LIMPA OS CAMPOS AO DUPLICAR 
+    fields.forEach(function(field) {
+        field.value = ""
+    })
+
+    document.querySelector('#schedule-items').appendChild(newfieldContainer)
 }
