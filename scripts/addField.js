a46fd1ee9c324854dc54a971265e432a6f617bab
roseliaguiar <62181263+roseliaguiar@users.noreply.github.com>
2020-08-05 14:03:35 -0300

    third day
    third day

---
+++ scripts/addField.js
@@ -0,0 +1,11 @@
+//PROUCURAR O BOTÃO
+//QUANDO CLICAR NO BOTÃO 
+//EXECUTAR UMA ACAO 
+//DUPLICAR OS CAMPOS 
+//COLOCAR NA PAGINA
+
+document.querySelector('#add-time').addEventListener('click', cloneField)
+
+function cloneField() {
+    document.querySelector('.schedule-item')
+}
